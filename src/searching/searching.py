97091dd21f070f8f6669b269ad3bc7c31b389c20
LeonChisum <leonchisum3306@gmail.com>
2020-09-12 12:25:37 -0400

    searching.py linear search complete, test passed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,12 @@
 def linear_search(arr, target):
     # Your code here
+    i = 0
 
-
+    while i < len(arr):
+        if arr[i] == target:
+            print("found")
+            return i
+        i = i + 1
     return -1   # not found
 
 
